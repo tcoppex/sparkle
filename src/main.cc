07e1a68de4f74d3743b73c4d9372e59d9d782537
tcoppex <tcoppex@gmail.com>
2018-04-13 18:55:07 +0200

    remove unused parameter

--- src/main.cc
+++ src/main.cc
@@ -7,7 +7,7 @@
 
 /* -------------------------------------------------------------------------- */
 
-int main(int argc, char *argv[]) {
+int main(int, char *[]) {
   App app;
 
   if (!app.init(WINDOW_TITLE)) {
