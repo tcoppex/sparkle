c54b691c153e9bb8fb2d2f89f82fda1cf66f7c8a
tcoppex <tcoppex@gmail.com>
2018-04-18 10:40:52 +0200

    Add specialized rotation functions with trigonometric value parameters.

--- src/shaders/sparkle/inc_math.glsl
+++ src/shaders/sparkle/inc_math.glsl
@@ -9,9 +9,7 @@ float TwoPi() {
   return 6.283185f;
 }
 
-mat3 rotationX(float alpha) {
-  const float c = cos(alpha);
-  const float s = sin(alpha);
+mat3 rotationX(float c, float s) {
   return mat3(
     vec3(1.0f, 0.0f, 0.0f),
     vec3(0.0f, c, s),
@@ -19,9 +17,7 @@ mat3 rotationX(float alpha) {
   );
 }
 
-mat3 rotationY(float alpha) {
-  const float c = cos(alpha);
-  const float s = sin(alpha);
+mat3 rotationY(float c, float s) {
   return mat3(
     vec3(c, 0.0f, -s),
     vec3(0.0f, 1.0f, 0.0f),
@@ -29,14 +25,24 @@ mat3 rotationY(float alpha) {
   );
 }
 
-mat3 rotationZ(float alpha) {
-  const float c = cos(alpha);
-  const float s = sin(alpha);
+mat3 rotationZ(float c, float s) {
   return mat3(
     vec3(c, s, 0.0f),
     vec3(-s, c, 0.0f),
     vec3(0.0f, 0.0f, 1.0f)
   );
+}
+
+mat3 rotationX(float radians) {
+  return rotationX(cos(radians), sin(radians));
+}
+
+mat3 rotationY(float radians) {
+  return rotationY(cos(radians), sin(radians));
+}
+
+mat3 rotationZ(float radians) {
+  return rotationZ(cos(radians), sin(radians));
 }
 
 #endif  // SHADERS_MATH_GLSL_
